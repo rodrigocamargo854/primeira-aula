2cfb7c2e279c377e595c4d1b63b2118530ac98e7
Entra21 <entra21@proway.treina>
2020-07-10 19:27:43 -0300

    .gitignore create

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace primeira_aula
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            Console.WriteLine("Qual é seu nome");
+
+            var age = 25 ;
+
+            var result  = Console.ReadLine( );
+
+            var price = 0.99;
+
+
+            Console.WriteLine($"Olá, {result} ");
+            Console.WriteLine(age);
+            Console.WriteLine();
+
+            var myBoolean = result == "Rodrigo";
+
+            Console.WriteLine(myBoolean);
+
+
+            
+
+
+
+
+
+            
+        }
+    }
+}
